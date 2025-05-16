8798340ffe06e633b74cceb69d0c754a6b4adf5e
Danila Platonov <danilaplatonov209@gmail.com>
2025-05-16 15:38:48 +0300

    add base user controller functionality

--- src/controllers/users/controller.ts
+++ src/controllers/users/controller.ts
@@ -1,29 +1,36 @@
 import { Request, Response } from 'express';
 import User from '../../models/user/user';
-import helpers from '../../helpers/user-helpers';
 
 class AuthController {
-    static async createUser(req: Request, res: Response) {
+    static async getUser(req: Request, res: Response) {
         try {
-            const user = await helpers.getUserFromToken(req);
+            const user = await User.findOne({ _id: req.query.id }, { password: 0 });
             res.status(200).json({ message: "Получение данных пользователя", user: user });
         }
         catch (error) {
-            res.status(400).json({ message: "Ошибка при создании пользователя" });
+            res.status(400).json({ message: "Ошибка при получении данных пользователя" });
             console.log(error);
         }
     }
     static async deleteUser(req: Request, res: Response) {
         try {
-            const user = await helpers.getUserFromToken(req);
-
-            if (user) {
-                await User.deleteOne({ _id: user?._id });
-                res.status(200).json({ message: "Успешное удаление данных пользователя" });
-            }
+            const userId = req.query.id;
+            await User.deleteOne({ _id: userId });
+            res.status(200).json({ message: "Успешное удаление данных пользователя" });
         }
         catch (error) {
             res.status(400).json({ message: "Ошибка при удалении данных о пользователе" });
+            console.log(error);
+        }
+    }
+    static async createUser(req: Request, res: Response) {
+        try {
+            const user = new User(req.body.user);
+            await user.save();
+            res.status(200).json({ message: "Успешное создание пользователя", user: user });
+        }
+        catch (error) {
+            res.status(400).json({ message: "Ошибка при создании пользователя" });
             console.log(error);
         }
     }
@@ -33,15 +40,6 @@ class AuthController {
         }
         catch (error) {
             res.status(400).json({ message: "Ошибка обновления данных о пользователе" });
-            console.log(error);
-        }
-    }
-    static async getUser(req: Request, res: Response) {
-        try {
-            
-        }
-        catch (error) {
-            res.status(400).json({ message: "Ошибка получения данных о пользователе" });
             console.log(error);
         }
     }
