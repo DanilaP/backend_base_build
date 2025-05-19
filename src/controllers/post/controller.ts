78a3b5b2ce13a92da7a2cf56b4e1ff042020a75d
Danila Platonov <danilaplatonov209@gmail.com>
2025-05-19 16:53:29 +0300

    add post deleting, updated fs-helpers with file deleting

--- src/controllers/post/controller.ts
+++ src/controllers/post/controller.ts
@@ -22,7 +22,21 @@ class PostsController {
     }
     static async deletePost(req: Request, res: Response) {
         try {
-            
+            const post = await Post.findOne({ _id: req.query.id });
+            await Post.deleteOne({ _id: req.query.id });
+
+            const postFilesURLs = post?.files.map(file => {
+                return file.url.replace(process.env.HOST_URL, `./static`);
+            }) as string[];
+
+            const result = await fsHelpers.removeFiles(postFilesURLs);
+
+            if (result.status === 200) {
+                res.status(200).json({ message: "Успешное удаление поста" });
+            }
+            else {
+                res.status(400).json({ message: "Ошибка при удалении файлов. Возможна потеря данных", post: post });
+            }
         }
         catch (error) {
             res.status(400).json({ message: "Ошибка при удалении поста" });
--- src/helpers/fs-helpers.ts
+++ src/helpers/fs-helpers.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 async function uploadFiles (files: any) {
     if (files && Object.keys(files).length !== 0) {
         const uploadedFiles = files.files.length > 0 ? files.files : [files.files];
@@ -25,5 +27,19 @@ async function uploadFiles (files: any) {
     return { filelist: [], status: 500 };
 }
 
+async function removeFiles (filesURL: string[]) {
+    let status = 200;
+    if (filesURL.length !== 0) {
+        filesURL.map(url => {
+            fs.unlink(url as string, (err) => {
+                if (err) {
+                    status = 500;
+                }
+            });
+        })
+    }
+    return { status };
+}
 
-export default { uploadFiles };
+
+export default { uploadFiles, removeFiles };
