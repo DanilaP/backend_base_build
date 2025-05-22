4e4f7ffd830016232cd84ea05a38150c6788fae4
Danila Platonov <danilaplatonov209@gmail.com>
2025-05-22 15:01:24 +0300

    add filters to user getting

--- src/controllers/users/controller.ts
+++ src/controllers/users/controller.ts
@@ -12,6 +12,17 @@ class UsersController {
             console.log(error);
         }
     }
+    static async getUsersWithFilters(req: Request, res: Response) {
+        try {
+            const filters = req.body;
+            const users = await User.find(filters);
+            res.status(200).json({ message: "Успешное получение фильтрованного списка пользователей", users });
+        }
+        catch (error) {
+            res.status(400).json({ message: "Ошибка получения фильтрованного списка пользователей" });
+            console.log(error);
+        }
+    }
 }
 
 export default UsersController;
--- src/controllers/users/router.ts
+++ src/controllers/users/router.ts
@@ -3,5 +3,6 @@ import UsersController from './controller';
 const router = Router();
 
 router.get('/', UsersController.getUsers);
+router.post('/', UsersController.getUsersWithFilters);
 
 export default router;
